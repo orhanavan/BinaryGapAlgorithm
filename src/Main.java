d79e814c5a15a631fa2348863d0725e7aa08e7fa
orhan <o.avan.71@gmail.com>
2018-11-17 01:30:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.solution(32);
+        System.out.println(result);
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+
+public class Solution {
+
+    public int solution(int N) {
+
+        // decimal to binary list
+        LinkedList<Integer> binary = new LinkedList<>();
+        while (N > 0) {
+            binary.add(N % 2);
+            N = N / 2;
+        }
+
+        int flag = 0;
+        int count = 0;
+        int max = 0;
+
+        for (int bit : binary) {
+
+            if (bit == 1) {
+                if (flag == 0) {
+                    flag = 1;
+                } else {
+                    if (count > max) {
+                        max = count;
+                        count = 0;
+                    }
+                }
+            } else {
+                if (flag == 1) {
+                    count++;
+                }
+
+            }
+        }
+
+        return max;
+    }
+}
